a7ab3d5dfd1a435b210a704497a96d8b4ed0c6c4
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2024-01-11 11:11:29 -0500

    9. Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,12 @@
 -- 9. Cities by States
-SELECT 
+/*SELECT 
     cities.id, 
     cities.name, 
     (SELECT name FROM states WHERE id = cities.state_id) AS state_name 
 FROM cities 
+ORDER BY cities.id ASC;*/
+
+SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+FROM cities
 ORDER BY cities.id ASC;
+
