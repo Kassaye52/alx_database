7de25dbdac488828d7470f96b76bc38b7078244e
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2024-01-10 21:53:00 -0500

    5. Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,8 @@
 -- 5. Full description:
-SELECT 
+/*SELECT 
     COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+    TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';*/
+SHOW CREATE TABLE first_table;
